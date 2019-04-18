e964581e8b25379702f3b55e85529122b32f148b
Aviv Rosenberg <aviv.rosenberg@gmail.com>
2019-04-19 00:33:53 +0300

    hw1: Update data generator
    Parametrize solution mu and std

--- hw1/data.py
+++ hw1/data.py
@@ -1,8 +1,9 @@
 import numpy as np
 
 
-def generate_dataset(n_samples, n_features, smax=10, smin=1,
-                     sample_std=10, noise_std=0.01):
+def generate_linear_regression(n, d, fullrank=True,
+                               smax=10, smin=1, sol_mu=100, sol_std=10,
+                               noise_std=0.01, **kw):
     """
     Generates a dataset for a linear regression optimization task:
 
@@ -10,21 +11,22 @@ def generate_dataset(n_samples, n_features, smax=10, smin=1,
         \min_{x\in\mathbb{R}} ||Ax - b||^2
 
     A and b.
-    A will be an (m,n) matrix with singular values between sigma_max to
-    sigma_min.
+    A will be an (n,d) matrix with singular values between smax to smin.
     b will be chosen as b = A*xs+n, where xs is some random point (the
     optimal solution) and n is uniform Gaussian noise with a given std.
 
-    :param n_samples: Number of rows in A (n)
-    :param n_features: Number of columns in A (also features in x, aka d)
+    :param n: Number of rows in A
+    :param d: Number of columns in A (also features in x)
+    :param fullrank: Whether or not A should be full rank.
     :param smax: Largest singular value
     :param smin: Smallest singular value
-    :param sample_std: Stdev. of distribution to sample xs from.
+    :param sol_mu: Mean of distribution to sample xs from.
+    :param sol_std: Stdev. of distribution to sample xs from.
     :param noise_std: Stdev. of Gaussian noise added to generate b.
     :return: A tuple containing A (n,d), b (n,), and xs (d,).
     """
 
-    n, d = n_samples, n_features
+    n, d = n, d
 
     # Decompose a random matrix T with SVD
     T = np.random.randn(n, d)
@@ -39,9 +41,10 @@ def generate_dataset(n_samples, n_features, smax=10, smin=1,
     A = U @ S @ Vh
 
     # Sample a random solution xs with (large variance)
-    xs = np.random.randn(d, 1) * 10
+    xs = np.random.randn(d, 1) * sol_std + sol_mu
 
     # Create bias vector
     b = A @ xs + np.random.randn(n, 1) * noise_std
 
     return A, b.reshape(-1), xs.reshape(-1)
+
