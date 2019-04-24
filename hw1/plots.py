a3c9a76511b678b059bdfebe773898f87187e0d2
Aviv Rosenberg <aviv.rosenberg@gmail.com>
2019-04-25 02:28:25 +0300

    hw1: update plots

--- hw1/plots.py
+++ hw1/plots.py
@@ -41,28 +41,23 @@ def plot_experiment(results: ExperimentResults, out_dir):
     cfg: ExperimentConfig = results.config
 
     for run_name, run_data in results.results_map.items():
-        t_axis = np.arange(run_data.shape[1])
+        t_axis = np.arange(1, run_data.shape[1]+1)
         mean = run_data[0]
         sterr = run_data[1]
 
-        ax.plot(t_axis, mean, label=run_name, linestyle='--', linewidth=1.)
+        ax.plot(t_axis, mean, label=run_name, linestyle='--', linewidth=0.7)
         ax.fill_between(t_axis, mean-sterr, mean+sterr, alpha=0.3)
 
     ax.set_xlabel(r'$t$')
-    ax.set_ylabel(r'$f(\mathbf{x_t})=\left\Vert'
-                  r'\mathbf{A}\mathbf{x_t}-\mathbf{b}'
-                  r'\right\Vert$')
+    ax.set_ylabel(r'$\vert f(\mathbf{x_t}) - f(\mathbf{x^{*}})\vert$')
     kappa = str(cfg.smax/cfg.smin) if cfg.smin > 0 else r'\infty'
     ax.set_title(rf'{cfg.name}, ($\kappa(A)={kappa}$)')
     ax.grid()
-    ax.legend()
-    ax.set_xlim(0, 200)
+    ax.legend(loc='upper right')
+    ax.set_xlim(auto=True)
+    ax.set_yscale('log')
+    ax.set_xscale('log')
 
     filename = os.path.join(out_dir, f'{cfg.name}')
     fmt = 'pdf'
     fig.savefig(f'{filename}.{fmt}', format=fmt)
-
-    ax.set_xlim(auto=True)
-    ax.set_yscale('log')
-    ax.set_xscale('log')
-    fig.savefig(f'{filename}_loglog.{fmt}', format=fmt)
