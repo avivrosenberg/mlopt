1cb83435f06ed8b50532d42ce6ffaa7bd91b73a2
Aviv Rosenberg <aviv.rosenberg@gmail.com>
2019-04-24 22:06:06 +0300

    hw1: improve plots

--- hw1/plots.py
+++ hw1/plots.py
@@ -4,7 +4,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 import hw1.config as hw1cfg
-from hw1.config import ExperimentResults
+from hw1.config import ExperimentResults, ExperimentConfig
 
 
 def plot_from_file(results_filename, out_dir):
@@ -38,23 +38,31 @@ def plot_experiment(results: ExperimentResults, out_dir):
     fig, ax = plt.subplots(1, 1)
     fig: plt.Figure = fig
     ax: plt.Axes = ax
+    cfg: ExperimentConfig = results.config
 
-    exp_name = results.config.name
     for run_name, run_data in results.results_map.items():
         t_axis = np.arange(run_data.shape[1])
         mean = run_data[0]
         sterr = run_data[1]
 
-        #ax.errorbar(t_axis, mean, yerr=sterr, label=run_name)
-        ax.plot(t_axis, mean, label=run_name)
+        ax.plot(t_axis, mean, label=run_name, linestyle='--', linewidth=1.)
         ax.fill_between(t_axis, mean-sterr, mean+sterr, alpha=0.3)
 
     ax.set_xlabel(r'$t$')
-    ax.set_ylabel(r'$f(\mathbf{x_t})$')
-    ax.set_title(exp_name)
-    ax.set_yscale('log')
+    ax.set_ylabel(r'$f(\mathbf{x_t})=\left\Vert'
+                  r'\mathbf{A}\mathbf{x_t}-\mathbf{b}'
+                  r'\right\Vert$')
+    kappa = str(cfg.smax/cfg.smin) if cfg.smin > 0 else r'\infty'
+    ax.set_title(rf'{cfg.name}, ($\kappa(A)={kappa}$)')
     ax.grid()
     ax.legend()
+    ax.set_xlim(0, 200)
 
-    filename = os.path.join(out_dir, f'{exp_name}.pdf')
-    fig.savefig(filename, format='pdf')
+    filename = os.path.join(out_dir, f'{cfg.name}')
+    fmt = 'pdf'
+    fig.savefig(f'{filename}.{fmt}', format=fmt)
+
+    ax.set_xlim(auto=True)
+    ax.set_yscale('log')
+    ax.set_xscale('log')
+    fig.savefig(f'{filename}_loglog.{fmt}', format=fmt)
