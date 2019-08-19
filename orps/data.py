e2193bb86fbbfabf877b1566b80bc15f18b74cbd
Aviv Rosenberg <aviv.rosenberg@gmail.com>
2019-08-20 01:21:51 +0300

    orps data: better indexing

--- orps/data.py
+++ orps/data.py
@@ -17,9 +17,9 @@ class EHazanPFDataset(object):
         mat = scipy.io.loadmat(self.ds_file)
 
         A = mat['A']  # 490x1000, contains prices of 490 stocks on 1000 days
-        B = mat['B']  # 491x1, contains filenames e.g. AAPL.csv
+        B = mat['B']  # 491x1, contains filenames e.g. 'AAPL.csv'
 
-        Bflat = np.hstack(B.flat)
+        Bflat = np.hstack(B.flat)  # flatten array of numpy arrays of string
         asset_names = map(lambda s: s.split('.')[0], Bflat)
 
         # We'll treat each asset as a feature (column) and each day as a
@@ -30,9 +30,12 @@ class EHazanPFDataset(object):
         return len(self._df)
 
     def __getitem__(self, item):
-        if isinstance(item, str):
+        if isinstance(item, (str, tuple)):
+            # Indexing with a string or tuple of strings will return
+            # features (columns).
             return self._df.loc[:, item].to_numpy()
         elif isinstance(item, (int, slice)):
+            # Indexing or slicing numerically will return a rows (samples).
             return self._df.iloc[item, :].to_numpy()
         else:
             raise ValueError(f"Unknown index type: {type(item)}")
