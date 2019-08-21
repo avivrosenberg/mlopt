bdd40d50c8fd865892e755f1119a574860c336f8
Aviv Rosenberg <aviv.rosenberg@gmail.com>
2019-08-22 00:31:36 +0300

    orps main script updates
    - Fix regret calculation
    - Save plots to file
    ;

--- orps_main.py
+++ orps_main.py
@@ -1,3 +1,4 @@
+import os
 import time
 
 import numpy as np
@@ -8,11 +9,9 @@ import orps.models
 
 
 def run_training():
+    # Load data
     ds = orps.data.EHazanPFDataset()
-
-    rounds = 1
     X = ds.to_numpy()
-    X = np.vstack((X,) * rounds)
 
     # Create asset returns
     R = (X[1:, :] / X[:-1, :])
@@ -27,7 +26,8 @@ def run_training():
         'OGD': orps.models.OGDOnlineRebalancingPortfolio(),
         'RFTL': orps.models.RFTLOnlineRebalancingPortfolio(),
         'Newton': orps.models.NewtonStepOnlineRebalancingPortfolio(),
-        'Best Fixed': orps.models.BestFixedRebalancingPortfolio(),
+        'Best Fixed': orps.models.BestFixedRebalancingPortfolio(
+            continuous=True, max_iter=25),
         'Best Single': orps.models.BestSingleAssetRebalancingPortfolio(),
     }
 
@@ -36,12 +36,11 @@ def run_training():
         print(f'Training {name}... ', end='')
         t = time.time()
         model.fit(R)
-        print(f'done in {time.time()-t:.3f} sec.')
-
+        print(f'done in {time.time() - t:.3f} sec.')
 
     # Plot wealth
     t_axis = np.arange(start=1, stop=T + 1)
-    plt.figure()
+    fig_wealth = plt.figure()
     for name, model in models.items():
         linespec = '--' if name.startswith("Best") else '-'
         plt.plot(t_axis, model.wealth(R), linespec, label=name)
@@ -51,16 +50,26 @@ def run_training():
     plt.legend()
 
     # Plot regret
-    plt.figure()
+    fig_regret = plt.figure()
+    Pstar = models['Best Fixed'].P_
     for name, model in models.items():
         if name.startswith("Best"):
             continue
-        plt.plot(t_axis, model.regret(models['Best Fixed'].p_), label=name)
-    # plt.plot(t_axis, np.log(t_axis), '--', label=r'$\log(t)$')
-    # plt.plot(t_axis, np.sqrt(t_axis), '--', label=r'$\sqrt{t}$')
-    plt.title('Regret')
+        plt.plot(t_axis, model.regret(R, Pstar, average=True), label=name)
+    # plt.plot(t_axis, np.log(t_axis)/t_axis, '--', label=r'$\log(t)/t$')
+    # plt.plot(t_axis, np.sqrt(t_axis)/t_axis, '--', label=r'$\sqrt{t}/t$')
+    plt.title('Average Regret')
     plt.xlabel('time (days)')
     plt.legend()
+
+    out_dir = os.path.join('out', 'orps')
+    os.makedirs(out_dir, exist_ok=True)
+    for fig in (fig_wealth, fig_regret):
+        fmt = 'pdf'
+        filename = os.path.join(out_dir, f'{fig.number}')
+        fig.set_size_inches(8*0.8, 6*0.8)
+        fig.savefig(f'{filename}.{fmt}', format=fmt,
+                    bbox_inches='tight', pad_inches=0.1)
 
     plt.show()
 
