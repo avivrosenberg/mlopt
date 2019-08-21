476a93ba55c36008bc34f890a951bd41f5023e9a
Aviv Rosenberg <aviv.rosenberg@gmail.com>
2019-08-22 00:57:19 +0300

    orps_main -> hw3_main
    reorganized and added some printouts.

--- hw3_main.py
+++ hw3_main.py
@@ -1,5 +1,6 @@
 import os
 import time
+import datetime as dt
 
 import numpy as np
 import matplotlib.pyplot as plt
@@ -8,7 +9,7 @@ import orps.data
 import orps.models
 
 
-def run_training():
+def orps_train():
     # Load data
     ds = orps.data.EHazanPFDataset()
     X = ds.to_numpy()
@@ -38,12 +39,37 @@ def run_training():
         model.fit(R)
         print(f'done in {time.time() - t:.3f} sec.')
 
-    # Plot wealth
-    t_axis = np.arange(start=1, stop=T + 1)
-    fig_wealth = plt.figure()
+    # Calculate wealth
+    print('Calculating wealth...', end='')
+    t = time.time()
+    wealth = {}
     for name, model in models.items():
+        wealth[name] = model.wealth(R)
+    print(f'done in {time.time() - t:.3f} sec.')
+
+    # Calculate regret
+    print('Calculating regret...', end='')
+    t = time.time()
+    regret = {}
+    Pstar = models['Best Fixed'].P_
+    for name, model in models.items():
+        if name.startswith("Best"):
+            continue
+        regret[name] = model.regret(R, Pstar, average=True)
+    print(f'done in {time.time() - t:.3f} sec.')
+
+    return models, wealth, regret
+
+
+def orps_plot(wealth: dict, regret: dict):
+    T, = next(iter(wealth.values())).shape
+    t_axis = np.arange(start=1, stop=T + 1)
+
+    # Plot wealth
+    fig_wealth = plt.figure()
+    for name, w in wealth.items():
         linespec = '--' if name.startswith("Best") else '-'
-        plt.plot(t_axis, model.wealth(R), linespec, label=name)
+        plt.plot(t_axis, w, linespec, label=name)
     plt.title('Wealth')
     plt.yscale('log')
     plt.xlabel('time (days)')
@@ -51,23 +77,21 @@ def run_training():
 
     # Plot regret
     fig_regret = plt.figure()
-    Pstar = models['Best Fixed'].P_
-    for name, model in models.items():
-        if name.startswith("Best"):
-            continue
-        plt.plot(t_axis, model.regret(R, Pstar, average=True), label=name)
+    for name, r in regret.items():
+        plt.plot(t_axis, r, label=name)
     # plt.plot(t_axis, np.log(t_axis)/t_axis, '--', label=r'$\log(t)/t$')
     # plt.plot(t_axis, np.sqrt(t_axis)/t_axis, '--', label=r'$\sqrt{t}/t$')
     plt.title('Average Regret')
     plt.xlabel('time (days)')
     plt.legend()
 
-    out_dir = os.path.join('out', 'orps')
+    timestamp = dt.datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
+    out_dir = os.path.join('out', 'orps', timestamp)
     os.makedirs(out_dir, exist_ok=True)
     for fig in (fig_wealth, fig_regret):
         fmt = 'pdf'
         filename = os.path.join(out_dir, f'{fig.number}')
-        fig.set_size_inches(8*0.8, 6*0.8)
+        fig.set_size_inches(8 * 0.8, 6 * 0.8)
         fig.savefig(f'{filename}.{fmt}', format=fmt,
                     bbox_inches='tight', pad_inches=0.1)
 
@@ -75,4 +99,9 @@ def run_training():
 
 
 if __name__ == '__main__':
-    run_training()
+    print('=== MLOPT HW3: Aviv Rosenberg & Yonatan Elul')
+    print('=== ========================================')
+
+    print('=== Runining online rebalancing portfolio selection...')
+    models, wealth, regret = orps_train()
+    orps_plot(wealth, regret)
